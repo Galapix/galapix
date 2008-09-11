45e9701964c9c3d91624500933548eb624a1bf14
Ingo Ruhnke <grumbel@gmail.com>
2008-09-11 13:07:59 +0000

    Some now unneeded offset code removed

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -88,19 +88,15 @@ Workspace::layout(float aspect_w, float aspect_h)
 
           images[i].set_target_scale(target_scale);
 
-          // Offset that positions the image in the center of the 1000x1000 rectangle
-          Vector2f off((1000.0f - images[i].get_original_width()  * target_scale) / 2,
-                       (1000.0f - images[i].get_original_height() * target_scale) / 2);
-
           if ((i/w) % 2 == 0)
             {
               images[i].set_target_pos(Vector2f((i % w) * 1024.0f,
-                                                (i / w) * 1024.0f) + off);
+                                                (i / w) * 1024.0f));
             }
           else
             {
               images[i].set_target_pos(Vector2f((w - (i % w)-1) * 1024.0f,
-                                                (i / w)         * 1024.0f) + off);
+                                                (i / w)         * 1024.0f));
             }
 
           next_pos = Vector2i(i % w, i / w);
