3a13d2d4f643a001e1830914d4010121a90ecf13
Ingo Ruhnke <grumbel@gmail.com>
2009-10-13 19:11:26 +0000

    Improved error messages for Zip

--- src/plugins/zip.cpp
+++ src/plugins/zip.cpp
@@ -19,9 +19,86 @@
 #include "plugins/zip.hpp"
 
 #include <stdexcept>
+#include <sstream>
 
 #include "util/exec.hpp"
 
+std::string zip_error_to_string(int err)
+{
+  // codes are taken from the unzip manpage
+  switch(err)
+  {
+    case 0: 
+      return "normal; no errors or warnings detected.";
+
+    case 1:
+      return "out one or more warning errors were encountered, but processing "
+        "completed successfully anyway. This includes zipfiles where one or "
+        "more files was skipped due to unsupported compression method or "
+        "encryption with an unknown password.";
+
+    case 2: 
+      return "a generic error in the zipfile format was detected.  Processing may "
+        "have completed successfully anyway; some broken zipfiles created by "
+        "other archivers have simple work-arounds.";
+
+    case 3: 
+      return "a severe error in the zipfile format was detected.  Processing "
+        "probably failed immediately.";
+
+    case 4:  
+      return "unzip was unable to allocate memory for one or more buffers during "
+        "program initialization.";
+
+    case 5:
+      return "unzip was unable to allocate memory or unable to obtain a tty to read "
+        "the decryption password(s).";
+
+    case 6:
+      return "unzip was unable to allocate memory during decompression to disk.";
+  
+    case 7: 
+      return "unzip was unable to allocate memory during in-memory decompression.";
+
+    case 8: 
+      return "[currently not used]";
+
+    case 9:  
+      return "the specified zipfiles were not found.";
+
+    case 10:
+      return "invalid options were specified on the command line.";
+
+    case 11:   
+      return "no matching files were found.";
+          
+    case 50: 
+      return "the disk is (or was) full during extraction.";
+
+    case 51:  
+      return "the end of the ZIP archive was encountered prematurely.";
+      break;                                                                 
+
+    case 80:  
+      return "the user aborted unzip prematurely with control-C (or similar)";
+
+    case 81:
+      return "testing or extraction of one or more files failed due to unsupported "
+        "compression methods or unsupported decryption.";
+
+    case 82:
+      return "no files were found due to bad decryption password(s).  (If even one "
+        "file is successfully processed, however, the exit status is 1.)";
+
+    default:
+    {
+      std::ostringstream out;
+      out << "unknown error code: "<< err;
+      return out.str();
+    }
+  }
+}
+
 void unzip_parse_line(std::vector<char>::const_iterator start, std::vector<char>::const_iterator end,
                       std::vector<std::string>& lst)
 {
@@ -78,7 +155,8 @@ Zip::get_filenames(const std::string& zip_filename)
 {
   Exec unzip("unzip");
   unzip.arg("-lqq").arg(zip_filename);
-  if (unzip.exec() == 0)
+  int zip_return_code = unzip.exec();
+  if (zip_return_code == 0)
   {
     std::vector<std::string> lst;
     unzip_parse_output(unzip.get_stdout().begin(), unzip.get_stdout().end(), lst);
@@ -86,7 +164,11 @@ Zip::get_filenames(const std::string& zip_filename)
   }
   else
   {
-    throw std::runtime_error("Zip: " + std::string(unzip.get_stderr().begin(), unzip.get_stderr().end()));
+    std::ostringstream out;
+    out << "Zip: " << unzip.str()
+        << "\n  zip-exit-code: " << zip_error_to_string(zip_return_code)
+        << "\n  " << std::string(unzip.get_stderr().begin(), unzip.get_stderr().end());
+    throw std::runtime_error(out.str());
   }
 }
 
@@ -95,54 +177,61 @@ Zip::get_file(const std::string& zip_filename, const std::string& filename)
 {
   Exec unzip("unzip");
   unzip.arg("-pqq").arg(zip_filename).arg(filename);
-  if (unzip.exec() == 0)
+  int zip_return_code = unzip.exec();
+  if (zip_return_code == 0)
   {
     // FIXME: Unneeded copy of data
     return Blob::copy(&*unzip.get_stdout().begin(), unzip.get_stdout().size());
   }
   else
   {
-    throw std::runtime_error("Zip: " + std::string(unzip.get_stderr().begin(), unzip.get_stderr().end()));
+    // "unzip -p" does not report any error messages at all, so we
+    // have to do them manually
+    std::ostringstream out;
+    out << "Zip: " << unzip.str()
+        << "\n  zip-exit-code: " << zip_error_to_string(zip_return_code)
+        << "\n  " << std::string(unzip.get_stderr().begin(), unzip.get_stderr().end());
+    throw std::runtime_error(out.str());
   }
 }
 
 #ifdef __TEST_ZIP__
 
-// g++ -Wall -Werror -ansi -pedantic blob.cpp exec.cpp zip.cpp -o myzip -D__TEST_ZIP__  
+  // g++ -Wall -Werror -ansi -pedantic blob.cpp exec.cpp zip.cpp -o myzip -D__TEST_ZIP__  
 
 #include <iostream>
 
-int main(int argc, char** argv)
-{
-  try
+  int main(int argc, char** argv)
   {
-    if (argc == 2)
+    try
     {
-      const std::vector<std::string>& files = Zip::get_filenames(argv[1]);
-      for(std::vector<std::string>::const_iterator i = files.begin(); i != files.end(); ++i)
+      if (argc == 2)
       {
-        std::cout << "File: '" << *i << "'" << std::endl;
+        const std::vector<std::string>& files = Zip::get_filenames(argv[1]);
+        for(std::vector<std::string>::const_iterator i = files.begin(); i != files.end(); ++i)
+        {
+          std::cout << "File: '" << *i << "'" << std::endl;
+        }
+      }
+      else if (argc == 3)
+      {
+        BlobHandle blob = Zip::get_file(argv[1], argv[2]);
+        blob.write_to_file("/tmp/out.file");
+        std::cout << "Writting /tmp/out.file" << std::endl;
+      }
+      else 
+      {
+        std::cout << "Usage: " << argv[0] << " ZIPFILE" << std::endl;
+        std::cout << "       " << argv[0] << " ZIPFILE FILETOEXTRACT" << std::endl;
       }
     }
-    else if (argc == 3)
+    catch(std::exception& err) 
     {
-      BlobHandle blob = Zip::get_file(argv[1], argv[2]);
-      blob.write_to_file("/tmp/out.file");
-      std::cout << "Writting /tmp/out.file" << std::endl;
+      std::cout << "Error: " << err.what() << std::endl;
     }
-    else 
-    {
-      std::cout << "Usage: " << argv[0] << " ZIPFILE" << std::endl;
-      std::cout << "       " << argv[0] << " ZIPFILE FILETOEXTRACT" << std::endl;
-    }
+    return 0;
   }
-  catch(std::exception& err) 
-  {
-    std::cout << "Error: " << err.what() << std::endl;
-  }
-  return 0;
-}
 
 #endif
 
-/* EOF */
+  /* EOF */
