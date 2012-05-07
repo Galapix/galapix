76eabf7c5ac1a96d8cade6607950d611ca8c1327
Ingo Ruhnke <grumbel@gmail.com>
2012-05-07 05:07:03 +0200

    Added missing operator<< for RowId

---
+++ src/database/row_id.cpp
@@ -0,0 +1,35 @@
+/*
+**  Galapix - an image viewer for large image collections
+**  Copyright (C) 2012 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "database/row_id.hpp"
+
+#include <ostream>
+
+std::ostream& operator<<(std::ostream& s, const RowId& id)
+{
+  if (id)
+  {
+    return s << "RowId(" << id.get_id() << ")";
+  }
+  else
+  {
+    return s << "RowId(none)";
+  }
+}
+
+/* EOF */
