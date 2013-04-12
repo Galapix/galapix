4fa587685f6723a0d63a7a19a30ced2a47e8e7d3
Ingo Ruhnke <grumbel@gmail.com>
2013-04-12 04:05:35 +0200

    Added string format() function

---
+++ src/util/format.hpp
@@ -0,0 +1,36 @@
+#ifndef HEADER_FORMAT_HPP
+#define HEADER_FORMAT_HPP
+
+#include <boost/format.hpp>
+#include <ostream>
+#include <string>
+
+inline void format(boost::format& fmt)
+{
+}
+
+template<typename Head, typename ...Rest> 
+inline void format(boost::format& fmt, const Head& head, Rest... rest)
+{
+  format(fmt % head, rest...);
+}
+
+template<typename ...Arg>
+inline std::string format(const std::string& str, Arg... arg)
+{
+  boost::format fmt(str);
+  format(fmt, arg...);
+  return fmt.str();
+}
+
+template<typename ...Arg>
+inline void format(std::ostream& out, const std::string& str, Arg... arg)
+{
+  boost::format fmt(str);
+  format(fmt, arg...);
+  out << fmt;
+}
+
+#endif
+
+/* EOF */
