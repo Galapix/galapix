Galapix/galapix

commit d1fbc4d34be0009f61c2c39cf97032983c70d15c

Changed assert() into a form that doesn't throw warnings on clang

src/job/thread_message_queue.hpp
@@ -72,7 +72,7 @@ public:
   C front()
   {
     std::unique_lock<std::mutex> lock(m_mutex);
-    assert(!m_values.empty());
+    assert(false && m_values.empty());
     C c(m_values.front());
     return c;
   }

src/server/database_thread.cpp
@@ -388,7 +388,7 @@ DatabaseThread::generate_tiles(const JobHandle& job_handle, const OldFileEntry& 
 {
   // FIXME: We are ignoring the callback, but shouldn't so assert in
   // case somebody tries to use one
-  assert(!callback);
+  assert(false && callback);
 
   int min_scale_in_db = -1;
   int max_scale_in_db = -1;

src/spnav/space_navigator.cpp
@@ -115,7 +115,7 @@ SpaceNavigator::start_thread()
 void
 SpaceNavigator::stop_thread()
 {
-  assert(!m_quit);
+  assert(false && m_quit);
 
   m_quit = true;
 

src/util/currenton.hpp
@@ -34,7 +34,7 @@ private:
   static C* s_current;
 
 protected:
-  Currenton()  { assert(!s_current); s_current = static_cast<C*>(this); }
+  Currenton()  { assert(false && s_current); s_current = static_cast<C*>(this); }
   virtual ~Currenton() { s_current = 0; }
 
 public:

src/database/file_tile_database.cpp
@@ -166,7 +166,7 @@ FileTileDatabase::store_tile(const RowId& image_id, const Tile& tile)
       break;
 
     default:
-      assert(!"Never reached");
+      assert(false && "Never reached");
   }
 }
 

src/database/statements/tile_entry_get_all_by_file_entry.hpp
@@ -68,7 +68,7 @@ public:
             break;
 
           default:
-            assert(!"never reached");
+            assert(false && "never reached");
         }
 
         tiles.push_back(tile);

src/database/statements/tile_entry_get_by_file_entry.hpp
@@ -68,7 +68,7 @@ public:
             break;
 
           default:
-            assert(!"never reached");
+            assert(false && "never reached");
         }
 
         return true;

src/database/statements/tile_entry_get_min_max_scale.hpp
@@ -52,7 +52,7 @@ public:
     }
     else
     {
-      assert(!"TileEntryGetMinMaxScale: never reached");
+      assert(false && "TileEntryGetMinMaxScale: never reached");
       return false;
     }
   }

commit c1f6dba37affcc4879f3969bbc8d1623e683c787

Fixed assert()s that got broken in d1fbc4d34be0009f61c2c39cf97032983c70d15c

src/job/thread_message_queue.hpp
@@ -72,7 +72,7 @@ public:
   C front()
   {
     std::unique_lock<std::mutex> lock(m_mutex);
-    assert(false && m_values.empty());
+    assert(!m_values.empty());
     C c(m_values.front());
     return c;
   }

src/server/database_thread.cpp
@@ -390,7 +390,7 @@ DatabaseThread::generate_tiles(const JobHandle& job_handle, const OldFileEntry& 
 {
   // FIXME: We are ignoring the callback, but shouldn't so assert in
   // case somebody tries to use one
-  assert(false && callback);
+  assert(!callback);
 
   int min_scale_in_db = -1;
   int max_scale_in_db = -1;

src/spnav/space_navigator.cpp
@@ -115,7 +115,7 @@ SpaceNavigator::start_thread()
 void
 SpaceNavigator::stop_thread()
 {
-  assert(false && m_quit);
+  assert(!m_quit);
 
   m_quit = true;
 

src/util/currenton.hpp
@@ -34,14 +34,14 @@ private:
   static C* s_current;
 
 protected:
-  Currenton()  { assert(false && s_current); s_current = static_cast<C*>(this); }
-  virtual ~Currenton() { s_current = 0; }
+  Currenton()  { assert(!s_current); s_current = static_cast<C*>(this); }
+  virtual ~Currenton() { s_current = nullptr; }
 
 public:
   static C& current() { assert(s_current); return *s_current; }
 };
 
-template<class C> C* Currenton<C>::s_current = 0;
+template<class C> C* Currenton<C>::s_current = nullptr;
 
 #endif
 