0b1df0b46b029411f03eab4aedbecf09893c2f85
Ingo Ruhnke <grumbel@gmail.com>
2015-08-11 08:14:15 +0200

    Some more SoftwareSurface test code

--- test/software_surface_test.cpp
+++ test/software_surface_test.cpp
@@ -27,4 +27,36 @@ TEST(SoftwareSurfaceTest, default_is_valid)
   surface.get_pixel_data();
 }
 
+TEST(SoftwareSurfaceTest, assignment)
+{
+  SoftwareSurface lhs(PixelData(PixelData::RGB_FORMAT, Size(32, 16)));
+  SoftwareSurface rhs(PixelData(PixelData::RGB_FORMAT, Size(64, 32)));
+  SoftwareSurface tmp;
+
+  tmp = lhs;
+  lhs = rhs;
+  rhs = tmp;
+
+  EXPECT_EQ(Size(64, 32), lhs.get_size());
+  EXPECT_EQ(Size(32, 16), rhs.get_size());
+  EXPECT_EQ(Size(32, 16), tmp.get_size());
+}
+
+TEST(SoftwareSurfaceTest, move)
+{
+  SoftwareSurface lhs(PixelData(PixelData::RGB_FORMAT, Size(32, 16)));
+  SoftwareSurface rhs(PixelData(PixelData::RGB_FORMAT, Size(64, 32)));
+  SoftwareSurface tmp;
+
+  tmp = std::move(lhs);
+  lhs = std::move(rhs);
+  rhs = std::move(tmp);
+
+  EXPECT_EQ(Size(64, 32), lhs.get_size());
+  EXPECT_EQ(Size(32, 16), rhs.get_size());
+
+  // this will crash as SoftwareSurface::m_pixel_data goes nullptr
+  // EXPECT_EQ(Size(32, 16), tmp.get_size());
+}
+
 /* EOF */
