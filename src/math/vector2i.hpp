74b25a3947377161edb2dc51f5d6522acc32e42c
Ingo Ruhnke <grumbel@gmail.com>
2019-01-14 18:57:18 +0100

    Change Copyright header style to something more compact

--- src/math/vector2i.hpp
+++ src/math/vector2i.hpp
@@ -1,4 +1,4 @@
- //  Pingus - A free Lemmings clone
+//  Pingus - A free Lemmings clone
 //  Copyright (C) 2008-2019 Ingo Ruhnke <grumbel@gmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
