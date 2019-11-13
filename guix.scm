9a46454d7f27956a79c53760d29f05ee85516442
Ingo Ruhnke <grumbel@gmail.com>
2019-11-13 16:30:23 +0100

    Update Guix build file to be IPFS compatible

--- guix.scm
+++ guix.scm
@@ -17,6 +17,10 @@
 ;; Install with:
 ;; guix package --install-from-file=guix.scm
 
+(set! %load-path
+      (cons* "/ipfs/QmafuNSaiqymsEvRECwQaz7TSz5hYMWiJ3X7Z3QGwuThPb/guix-cocfree_v0.0.0-37-g291d4eb"
+             %load-path))
+
 (use-modules (ice-9 popen)
              (ice-9 rdelim)
              (guix build utils)
@@ -37,83 +41,62 @@
              (gnu packages python)
              (gnu packages sdl)
              (gnu packages sqlite)
-             )
+             (guix-cocfree utils))
 
 (define %source-dir (dirname (current-filename)))
 
-(define current-commit
-  (with-directory-excursion %source-dir
-   (let* ((port   (open-input-pipe "git describe --tags"))
-          (output (read-line port)))
-     (close-pipe port)
-     (string-trim-right output #\newline))))
+(define-public galapix-0.3
+  (package
+   (name "galapix-0.3")
+   (version (version-from-source %source-dir))
+   (source (local-file %source-dir
+                       #:recursive? #t
+                       #:select? (source-select-function %source-dir
+                                                         '(""
+                                                           "/external/benchmark"
+                                                           "/external/glm"
+                                                           "/external/googletest"
+                                                           "/external/logmich"
+                                                           "/external/sexp-cpp"
+                                                           "/external/uitest"))))
+   (arguments
+    `(#:tests? #f
+      #:configure-flags '()
+      #:phases (modify-phases
+                %standard-phases
+                (add-before 'configure 'fixgcc9
+                            (lambda _
+                              (unsetenv "C_INCLUDE_PATH")
+                              (unsetenv "CPLUS_INCLUDE_PATH")))
+                (add-before 'configure 'patch-python
+                            (lambda _
+                              (substitute* "bin2h.py"
+                                           (("/usr/bin/env python") (which "python3")))
+                              #t))
 
-;;           (add-before 'configure 'fixgcc7
-;;             (lambda _
-;;               (unsetenv "C_INCLUDE_PATH")
-;;               (unsetenv "CPLUS_INCLUDE_PATH")))
+                )))
+   (build-system cmake-build-system)
+   (inputs `(("python" ,python)
+             ("sdl2" ,sdl2)
+             ("sdl2-image" ,sdl2-image)
+             ("sdl2-mixer" ,sdl2-mixer)
+             ("mesa" ,mesa)
+             ("glew" ,glew)
+             ("curl" ,curl)
+             ("boost" ,boost)
+             ("sqlite" ,sqlite)
+             ("libmhash" ,libmhash)
+             ("imagemagick" ,imagemagick)
+             ("libexif" ,libexif)
+             ("coreutils" ,coreutils)
+             ))
+   (native-inputs `(("pkg-config" ,pkg-config)
+                    ("gcc" ,gcc-9)))
+   (synopsis "An image viewer for large image collections")
+   (description "An image viewer for large image collections")
+   (home-page "https://gitlab.com/galapix/galapix")
+   (license gpl3+)))
 
-(define (source-predicate . dirs)
-  (let ((preds (map (lambda (p)
-                      (git-predicate (string-append %source-dir p)))
-                    dirs)))
-    (lambda (file stat)
-      (let loop ((f (car preds))
-                 (rest (cdr preds)))
-        (if (f file stat)
-            #t
-            (if (not (nil? rest))
-                (loop (car rest) (cdr rest))
-                #f))))))
-
-(package
- (name "galapix")
- (version current-commit)
- (source (local-file %source-dir
-                     #:recursive? #t
-                     #:select? (source-predicate ""
-                                                 "/external/benchmark"
-                                                 "/external/glm"
-                                                 "/external/googletest"
-                                                 "/external/logmich"
-                                                 "/external/sexp-cpp"
-                                                 "/external/uitest")))
- (arguments
-  `(#:tests? #f
-    #:configure-flags '()
-    #:phases (modify-phases
-              %standard-phases
-              (add-before 'configure 'fixgcc9
-                          (lambda _
-                            (unsetenv "C_INCLUDE_PATH")
-                            (unsetenv "CPLUS_INCLUDE_PATH")))
-              (add-before 'configure 'patch-python
-                          (lambda _
-                            (substitute* "bin2h.py"
-                                         (("/usr/bin/env python") (which "python3")))
-                            #t))
-
-              )))
- (build-system cmake-build-system)
- (inputs `(("python" ,python)
-           ("sdl2" ,sdl2)
-           ("sdl2-image" ,sdl2-image)
-           ("sdl2-mixer" ,sdl2-mixer)
-           ("mesa" ,mesa)
-           ("glew" ,glew)
-           ("curl" ,curl)
-           ("boost" ,boost)
-           ("sqlite" ,sqlite)
-           ("libmhash" ,libmhash)
-           ("imagemagick" ,imagemagick)
-           ("libexif" ,libexif)
-           ("coreutils" ,coreutils)
-           ))
- (native-inputs `(("pkg-config" ,pkg-config)
-                  ("gcc" ,gcc-9)))
- (synopsis "An image viewer for large image collections")
- (description "An image viewer for large image collections")
- (home-page "https://gitlab.com/galapix/galapix")
- (license gpl3+))
+galapix-0.3
 
 ;; EOF ;;
