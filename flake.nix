ada64b8b693d8a218aa2e295be1f8c2ed469aed1
Ingo Ruhnke <grumbel@gmail.com>
2021-11-11 21:52:46 +0100

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "An image viewer for large image collections";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          galapix = pkgs.stdenv.mkDerivation {
+            pname = "galapix";
+            version = "0.2.2";
+            src = nixpkgs.lib.cleanSource ./.;
+            enableParallelBuilding = true;
+            sconsFlags = [
+              "GALAPIX_SDL=True"
+              "GALAPIX_GTK=False"
+            ];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/galapix.sdl $out/bin/
+            '';
+            postFixup = ''
+                wrapProgram $out/bin/galapix.sdl \
+                  --prefix LIBGL_DRIVERS_PATH ":" "${pkgs.mesa.drivers}/lib/dri" \
+                  --prefix LD_LIBRARY_PATH ":" "${pkgs.mesa.drivers}/lib"
+            '';
+            nativeBuildInputs = [
+              pkgs.scons
+              pkgs.gcc
+              pkgs.pkgconfig
+              pkgs.makeWrapper
+            ];
+            buildInputs = [
+              pkgs.SDL2
+              pkgs.SDL2_image
+              pkgs.boost
+              pkgs.curl
+              pkgs.glew
+              pkgs.imagemagick6
+              pkgs.libGL
+              pkgs.libGLU
+              pkgs.libexif
+              pkgs.libmhash
+              pkgs.libspnav
+              pkgs.sqlite
+            ];
+           };
+        };
+        defaultPackage = packages.galapix;
+      });
+}
