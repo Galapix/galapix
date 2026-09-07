28e796eb3caff7a98955c5900f5abf2af36df55c
Ingo Ruhnke <grumbel@gmail.com>
2026-09-07 10:55:31 +0000

    master cleanup: VERSION 0.2.2, drop broken galapix_gtk flake app
    - Add VERSION (0.2.2); flake reads it for the package version
    - Remove apps.galapix_gtk (binary was never built under Nix)
    - cmakeFlags BUILD_GALAPIX_GTK=OFF; CMake default OFF
    - Assert galapix.sdl is installed; set meta.mainProgram
    - NEWS + README note: Nix ships SDL only; Gtk2 needs missing deps
    
    Co-authored-by: Grok <grok@x.ai>

--- flake.nix
+++ flake.nix
@@ -13,33 +13,46 @@
   outputs = { self, nixpkgs, flake-utils, vidthumb }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # pkgs = nixpkgs.legacyPackages.${system};
         pkgs = import nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
         };
+        version =
+          nixpkgs.lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION);
       in rec {
         packages = rec {
           default = galapix;
 
           galapix = pkgs.stdenv.mkDerivation {
             pname = "galapix";
-            version = "0.2.2";
+            inherit version;
             src = nixpkgs.lib.cleanSource ./.;
+
+            meta = with pkgs.lib; {
+              description = "An image viewer for large image collections";
+              homepage = "https://github.com/Galapix/galapix";
+              license = licenses.gpl3Plus;
+              mainProgram = "galapix.sdl";
+              platforms = platforms.linux;
+            };
 
             # Disabled due to insecure qtwebkit dependency
             # --set GALAPIX_KOCONVERTER "${pkgs.calligra}/bin/koconverter"
             postFixup = ''
-                wrapProgram $out/bin/galapix.sdl \
-                  --set GALAPIX_RAR "${pkgs.rar}/bin/rar" \
-                  --set GALAPIX_RSVG "${pkgs.librsvg}/bin/rsvg" \
-                  --set GALAPIX_7ZR "${pkgs.p7zip}/bin/7zr" \
-                  --set GALAPIX_TAR "${pkgs.gnutar}/bin/tar" \
-                  --set GALAPIX_UFRAW_BATCH "${pkgs.nufraw}/bin/nufraw-batch" \
-                  --set GALAPIX_VIDTHUMB "${vidthumb.packages.${system}.default}/bin/vidthumb" \
-                  --set GALAPIX_XCFINFO "${pkgs.xcftools}/bin/xcfinfo" \
-                  --set GALAPIX_XCF2PNG "${pkgs.xcftools}/bin/xcf2png" \
-                  --set GALAPIX_UNZIP "${pkgs.unzip}/bin/unzip"
+              if [ ! -x "$out/bin/galapix.sdl" ]; then
+                echo "error: galapix.sdl was not installed" >&2
+                exit 1
+              fi
+              wrapProgram $out/bin/galapix.sdl \
+                --set GALAPIX_RAR "${pkgs.rar}/bin/rar" \
+                --set GALAPIX_RSVG "${pkgs.librsvg}/bin/rsvg" \
+                --set GALAPIX_7ZR "${pkgs.p7zip}/bin/7zr" \
+                --set GALAPIX_TAR "${pkgs.gnutar}/bin/tar" \
+                --set GALAPIX_UFRAW_BATCH "${pkgs.nufraw}/bin/nufraw-batch" \
+                --set GALAPIX_VIDTHUMB "${vidthumb.packages.${system}.default}/bin/vidthumb" \
+                --set GALAPIX_XCFINFO "${pkgs.xcftools}/bin/xcfinfo" \
+                --set GALAPIX_XCF2PNG "${pkgs.xcftools}/bin/xcf2png" \
+                --set GALAPIX_UNZIP "${pkgs.unzip}/bin/unzip"
             '';
 
             nativeBuildInputs = with pkgs; [
@@ -62,16 +75,21 @@
               libspnav
               sqlite
             ];
-           };
+
+            # SDL only under Nix. Gtk2 needs libglademm/gtkglextmm, which are
+            # no longer in nixpkgs; the incomplete Gtk UI is optional via CMake
+            # on systems that still provide those packages.
+            cmakeFlags = [
+              "-DBUILD_GALAPIX_SDL=ON"
+              "-DBUILD_GALAPIX_GTK=OFF"
+            ];
+          };
         };
+
         apps = rec {
           galapix_sdl = flake-utils.lib.mkApp {
             drv = packages.galapix;
             exePath = "/bin/galapix.sdl";
-          };
-          galapix_gtk = flake-utils.lib.mkApp {
-            drv = packages.galapix;
-            exePath = "/bin/galapix.gtk";
           };
           default = galapix_sdl;
         };
